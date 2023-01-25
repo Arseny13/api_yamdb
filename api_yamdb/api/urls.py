74438474fb5ba58fee349520ae8e12d8e0e35bc3
last-ui <slastukhin_av@mail.ru>
2023-01-25 23:36:41 +0700

    Сменил имя вьюхи на send_email, из-за конфликта имен с импортируемой фукцией send_mail

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,14 +4,14 @@ from rest_framework import routers
 
 from . import views
 from .views import (APIUser, CategoryViewSet, GenreViewSet, TitleViewSet,
-                    UserViewSet, get_token, send_mail)
+                    UserViewSet, get_token, send_email)
 
 app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/',
+    r'titles/(?P<title_id>\d+)/reviews',
     views.ReviewViewSet,
     basename='review'
 )
@@ -28,7 +28,7 @@ router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 
 urlpatterns = [
-    path('v1/auth/signup/', send_mail, name='signup'),
+    path('v1/auth/signup/', send_email, name='signup'),
     path('v1/auth/token/', get_token, name='get_token'),
     path('v1/users/me/', APIUser.as_view()),
     path('v1/', include(router_v1.urls)),
